60b41d6dca08815fe3a0204fa1301991b7860903
Aryadne Guardieiro <aryadne.guardieiro@ufu.br>
2019-07-02 15:08:41 -0300

    fixing issues

--- kafka_influxdb/encoder/json_encoder.py
+++ kafka_influxdb/encoder/json_encoder.py
@@ -29,7 +29,6 @@ class Encoder(object):
          "tags":        {
                            "tag1": "value1"
                         }
-         
        }
     ]
     """
@@ -44,18 +43,19 @@ class Encoder(object):
             except ValueError as e:
                 logging.debug("Error in encoder: %s", e)
                 continue
-            pdb.set_trace()
-            measurement = Encoder.format_measurement_name(
-                entry, 'measurement')
-            value = Encoder.format_value(entry)
-            time = Encoder.format_time(entry)
-            tags = Encoder.format_tags(
-                entry, ['tags'])
-            measurements.append(Encoder.compose_data(
-                measurement, tags, value, time))
-        except Exception as e:
-            logging.debug("Error in input data: %s. Skipping.", e)
-            continue
+            try:
+                pdb.set_trace()
+                measurement = Encoder.format_measurement_name(
+                    entry, 'measurement')
+                value = Encoder.format_value(entry)
+                time = Encoder.format_time(entry)
+                tags = Encoder.format_tags(
+                    entry, ['tags'])
+                measurements.append(Encoder.compose_data(
+                    measurement, tags, value, time))
+                except Exception as e:
+                    logging.debug("Error in input data: %s. Skipping.", e)
+                    continue
         return measurements
 
     @staticmethod
