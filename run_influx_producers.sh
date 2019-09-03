aa5d45244b0d77340d0b2fa0ceb316dd3ccaef8c
Aryadne Guardieiro <aryadne.guardieiro@ufu.br>
2019-09-03 16:02:57 -0300

    fixing file path to run from any directory

--- run_influx_producers.sh
+++ run_influx_producers.sh
@@ -1,11 +1,12 @@
 DURATION=$1
+SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 echo "Running config_client_metrics";
-timeout "$DURATION"m python3 -m kafka_influxdb -c config_client_metrics.yaml &
+timeout "$DURATION"m python3 -m kafka_influxdb -c SCRIPT_PATH/config_client_metrics.yaml &
 echo "Running config_estimate_sla_sro";
-timeout "$DURATION"m python3 -m kafka_influxdb -c config_estimate_sla_sro.yaml &
+timeout "$DURATION"m python3 -m kafka_influxdb -c SCRIPT_PATH/config_estimate_sla_sro.yaml &
 echo "Running config_loadgen_metrics.yaml"
-timeout "$DURATION"m python3 -m kafka_influxdb -c config_loadgen_metrics.yaml &
+timeout "$DURATION"m python3 -m kafka_influxdb -c SCRIPT_PATH/config_loadgen_metrics.yaml &
 echo "Running config_selected_metrics.yaml"
-timeout "$DURATION"m python3 -m kafka_influxdb -c config_selected_metrics.yaml &
+timeout "$DURATION"m python3 -m kafka_influxdb -c SCRIPT_PATH/config_selected_metrics.yaml &
 echo "Done"
