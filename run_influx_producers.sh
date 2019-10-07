55070b02eda713cdc421a85e97b17c3008918953
Aryadne <aryadneccomp@gmail.com>
2019-10-07 14:35:34 +0000

    solving conflicts

--- run_influx_producers.sh
+++ run_influx_producers.sh
@@ -15,6 +15,8 @@ echo "Running config_client_metrics";
 timeout "$DURATION"m python3 -m kafka_influxdb -c config_client_metrics.yaml &
 echo "Running config_estimate_sla_sro";
 timeout "$DURATION"m python3 -m kafka_influxdb -c config_estimate_sla_sro.yaml &
+echo "Running config_logs_sro";
+timeout "$DURATION"m python3 -m kafka_influxdb -c config_logs_sro.yaml &
 echo "Running config_loadgen_metrics.yaml"
 timeout "$DURATION"m python3 -m kafka_influxdb -c config_loadgen_metrics.yaml &
 echo "Running config_selected_metrics.yaml"
